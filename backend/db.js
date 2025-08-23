dc10ba20171aaa1bb33b3ec0bdf9d15987806565
fede1817 <frayg52@gmail.com>
2025-08-23 16:37:31 -0300

    prueba

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "postgres",
-  host: "192.168.100.87",
-  database: "sanjorge",
+  host: "192.168.139.35",
+  database: "san-jorge",
   password: "123",
   port: 5432,
 });
